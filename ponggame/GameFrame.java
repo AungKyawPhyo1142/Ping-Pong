deae3c7a59f0edb2094bca3e09a12746cc1b680c
Aung Kyaw Phyo <94116245+AungKyawPhyo1142@users.noreply.github.com>
2022-02-23 11:45:56 +0630

    Add files via upload

---
+++ ponggame/GameFrame.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ponggame;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+	GamePanel panel;
+	
+	GameFrame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
